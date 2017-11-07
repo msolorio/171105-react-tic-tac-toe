19d8635cc4d6fc35d71b237d9b32e06d367743db
Solorio <Michael_Solorio@intuit.com>
2017-11-07 12:56:32 -0800

    add calculate winner logic

--- src/components/App.js
+++ src/components/App.js
@@ -14,8 +14,32 @@ class App extends Component {
     this.handleSquareClick = this.handleSquareClick.bind(this);
   }
 
+  calculateWinner() {
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [6, 4, 2]
+    ];
+
+    const squareVals = this.state.squareVals;
+
+    return winningCombos.reduce((winner, combo) => {
+      const [a, b, c] = combo;
+      if (squareVals[a] === squareVals[b] && squareVals[a] === squareVals[c]) {
+        return squareVals[a];
+      }
+      return winner;
+    }, null);
+  }
+
   handleSquareClick(index) {
-    console.log('in handleSquareClick:', index);
+    if (this.calculateWinner()) return;
+    if (this.state.squareVals[index] !== null) return;
 
     const squareValsClone = this.state.squareVals.slice();
     squareValsClone[index] = this.state.xIsNext ? 'X' : 'O';
@@ -29,7 +53,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Message />
+        <Message winner={this.calculateWinner()} xIsNext={this.state.xIsNext} />
         <Board squareVals={this.state.squareVals}
           onSquareClick={this.handleSquareClick} />
       </div>
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 export default function Message(props) {
+
+  console.log('props.winner:', props.winner);
+
+  function getMessage() {
+    if (props.winner) {
+      return `Winner: ${props.winner}`;
+    }
+    return `Player's turn: ${props.xIsNext ? 'X' : 'O'}`;
+  }
+
   return (
-    <div className="Message">from Message component</div>
+    <div className="Message">{getMessage()}</div>
   );
 }
